65d1039a375266d5756a1ccac147368475ec7c0f
Lena Karlova <o.karlova@internetdevels.com>
2017-06-02 03:15:16 +0300

    Styles

--- sites/all/themes/kittens_theme/templates/page.tpl.php
+++ sites/all/themes/kittens_theme/templates/page.tpl.php
@@ -11,42 +11,56 @@
 <header class="header" role="banner">
   <div class="layout-center">
 
-  <?php if ($logo): ?>
-    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-  <?php endif; ?>
+    <?php if ($site_name || $site_slogan): ?>
+      <div class="header__name-and-slogan">
+        <?php if ($site_name): ?>
+          <h1 class="header__site-name">
+            <a href="<?php print $front_page; ?>"
+               title="<?php print t('Home'); ?>" class="header__site-link"
+               rel="home"><span><?php print $site_name; ?></span></a>
+          </h1>
+        <?php endif; ?>
 
-  <?php if ($site_name || $site_slogan): ?>
-    <div class="header__name-and-slogan">
-      <?php if ($site_name): ?>
-        <h1 class="header__site-name">
-          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-        </h1>
-      <?php endif; ?>
+        <?php if ($site_slogan): ?>
+          <div class="header__site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
 
-      <?php if ($site_slogan): ?>
-        <div class="header__site-slogan"><?php print $site_slogan; ?></div>
-      <?php endif; ?>
-    </div>
-  <?php endif; ?>
+    <?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"
+         rel="home" class="header__logo"><img src="<?php print $logo; ?>"
+                                              alt="<?php print t('Home'); ?>"
+                                              class="header__logo-image"/></a>
+    <?php endif; ?>
 
-  <?php if ($secondary_menu): ?>
-    <nav class="header__secondary-menu" role="navigation">
-      <?php print theme('links__system_secondary_menu', array(
-        'links' => $secondary_menu,
-        'attributes' => array(
-          'class' => array('links', 'inline', 'clearfix'),
-        ),
-        'heading' => array(
-          'text' => $secondary_menu_heading,
-          'level' => 'h2',
-          'class' => array('visually-hidden'),
-        ),
-      )); ?>
-    </nav>
-  <?php endif; ?>
+    <?php if ($main_menu): ?>
+      <nav class="header__main-menu" role="navigation">
+        <?php
+        // This code snippet is hard to modify. We recommend turning off the
+        // "Main menu" on your sub-theme's settings form, deleting this PHP
+        // code block, and, instead, using the "Menu block" module.
+        // @see https://drupal.org/project/menu_block
+        print theme('links__system_main_menu', [
+          'links' => $main_menu,
+          'attributes' => [
+            'class' => ['navbar', 'clearfix'],
+          ],
+          'heading' => [
+            'text' => t('Main menu'),
+            'level' => 'h2',
+            'class' => ['visually-hidden'],
+          ],
+        ]); ?>
+      </nav>
+    <?php endif; ?>
 
-  <?php print render($page['header']); ?>
+    <?php print render($page['navigation']); ?>
+
+    <?php print render($page['header']); ?>
   </div>
+
+
 </header>
 
 <div class="layout-center">
@@ -54,29 +68,30 @@
   <div class="layout-3col layout-swap">
 
     <?php
-      // Render the sidebars to see if there's anything in them.
-      $sidebar_first  = render($page['sidebar_first']);
-      $sidebar_second = render($page['sidebar_second']);
-      // Decide on layout classes by checking if sidebars have content.
-      $content_class = 'layout-3col__full';
-      $sidebar_first_class = $sidebar_second_class = '';
-      if ($sidebar_first && $sidebar_second):
-        $content_class = 'layout-3col__right-content';
-        $sidebar_first_class = 'layout-3col__first-left-sidebar';
-        $sidebar_second_class = 'layout-3col__second-left-sidebar';
-      elseif ($sidebar_second):
-        $content_class = 'layout-3col__left-content';
-        $sidebar_second_class = 'layout-3col__right-sidebar';
-      elseif ($sidebar_first):
-        $content_class = 'layout-3col__right-content';
-        $sidebar_first_class = 'layout-3col__left-sidebar';
-      endif;
+    // Render the sidebars to see if there's anything in them.
+    $sidebar_first = render($page['sidebar_first']);
+    $sidebar_second = render($page['sidebar_second']);
+    // Decide on layout classes by checking if sidebars have content.
+    $content_class = 'layout-3col__full';
+    $sidebar_first_class = $sidebar_second_class = '';
+    if ($sidebar_first && $sidebar_second):
+      $content_class = 'layout-3col__right-content';
+      $sidebar_first_class = 'layout-3col__first-left-sidebar';
+      $sidebar_second_class = 'layout-3col__second-left-sidebar';
+    elseif ($sidebar_second):
+      $content_class = 'layout-3col__left-content';
+      $sidebar_second_class = 'layout-3col__right-sidebar';
+    elseif ($sidebar_first):
+      $content_class = 'layout-3col__right-content';
+      $sidebar_first_class = 'layout-3col__left-sidebar';
+    endif;
     ?>
 
     <main class="<?php print $content_class; ?>" role="main">
       <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a href="#skip-link" class="visually-hidden visually-hidden--focusable" id="main-content">Back to top</a>
+      <?php //print $breadcrumb; ?>
+      <a href="#skip-link" class="visually-hidden visually-hidden--focusable"
+         id="main-content">Back to top</a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
         <h1><?php print $title; ?></h1>
@@ -92,35 +107,6 @@
       <?php print $feed_icons; ?>
     </main>
 
-    <div class="layout-swap__top layout-3col__full">
-
-      <a href="#skip-link" class="visually-hidden visually-hidden--focusable" id="main-menu" tabindex="-1">Back to top</a>
-
-      <?php if ($main_menu): ?>
-        <nav class="main-menu" role="navigation">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see https://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('navbar', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('visually-hidden'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
-
-      <?php print render($page['navigation']); ?>
-
-    </div>
-
     <?php if ($sidebar_first): ?>
       <aside class="<?php print $sidebar_first_class; ?>" role="complementary">
         <?php print $sidebar_first; ?>
@@ -134,9 +120,9 @@
     <?php endif; ?>
 
   </div>
-
-  <?php print render($page['footer']); ?>
-
+  <a href="#skip-link" class="visually-hidden visually-hidden--focusable"
+     id="main-menu" tabindex="-1">Back to top</a>
 </div>
+<?php print render($page['footer']); ?>
 
 <?php print render($page['bottom']); ?>
